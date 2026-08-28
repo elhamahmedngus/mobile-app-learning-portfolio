c46d3805788621fe6159590297552214ba124f04
elhamahmedngus <elhamahmedngus@gmail.com>
2026-08-28 19:31:28 +0800

    Create intro-to-kotlin.kt with example functions
    Added multiple main functions demonstrating Kotlin syntax and printing.

---
+++ Module-1/Module-1/Source Code/intro-to-kotlin.kt
@@ -0,0 +1,35 @@
+Exercises
+
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+}
+
+fun main() {
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+}
+
+fun main() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
+fun main() {
+    println("Tomorrow is rainy")
+}
+
+fun main() {
+    println("There is a chance of snow")
+}
+
+fun main() {
+    println("How's the weather today?")
+}
+
+
